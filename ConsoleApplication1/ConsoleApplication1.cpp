77f4fef80909c514f86fa439acf36679b24e7e69
Szymon Dąbrowski <szydab1@st.amu.edu.pl>
2017-10-20 16:19:43 +0200

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+#include<iostream>
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+    return 0;
+}
+
